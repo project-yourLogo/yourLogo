c7dac36556f6def6c7da57304d3270a51d342b08
project-yourLogo <66630200+project-yourLogo@users.noreply.github.com>
2020-06-11 23:28:20 +0300

    added

--- js/script.js
+++ js/script.js
@@ -8,3 +8,42 @@ function addClass(selList, selBurger){
     })
 }
 addClass('.header__menu', '.header__burger')
+;
+const slider = (slides, prew, next) => {
+    let index = 1
+    const items = document.querySelectorAll(slides),
+          prewBtn = document.querySelector(prew),
+          nextBtn = document.querySelector(next);
+    
+    function showSlides (n){
+        if(n > items.length){
+            index = 1
+        }
+        
+        if(n < 1){
+            index = items.length
+        }
+
+        items.forEach(item => {
+            item.style.display = 'none'
+        })
+
+        items[index - 1].style.display = 'flex'
+    }
+
+    showSlides(index)
+
+    function plusSlides(n){
+        showSlides(index += n)
+    }
+
+    prewBtn.addEventListener('click', () => {
+        plusSlides(-1)
+    })
+
+    nextBtn.addEventListener('click', () => {
+        plusSlides(1)
+    })
+
+}
+slider('.slider-item', '.slider__next', '.slider__prew');
